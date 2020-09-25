795c4746106c6eefb24766a50992cf9b424636cc
Sakti Dewantoro <saktyd@gmail.com>
2020-09-25 11:23:43 +0700

    create header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="slack">
-      <h1>Hello world</h1>
+      <Header/>
     </div>
   );
 }
