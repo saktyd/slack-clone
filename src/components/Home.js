d5bbff4b93c0fc8358a0d66fafa1d268f46feb86
Sakti Dewantoro <saktyd@gmail.com>
2020-10-06 14:44:28 +0700

    add content on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../style/Home.css'
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to Slack Clone App</h1>
+        <div className="home">
+            <h1>Welcome to Slack Clone</h1> <br/>
+            <span> This app just a prototype for portfolio<br/> <br/> <br/>
+                <b>Listing of Features :</b> <br/>
+                Sign In <br/>
+                Create channel <br/>
+                Read & Send Message on selected channel <br/>
+                Sign Out <br/>
+            </span>
         </div>
     )
 }
